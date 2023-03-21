eec4633b2b5c68d43b7aff97d8ee724f915f6547
Abdul Razzak <abdulrazzak74832@gmail.com>
2023-03-21 13:44:01 +0530

    set the fetch exercises in Home Component

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
-
-const ExerciseCard = () => {
+import { Button, Stack, Typography } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+const ExerciseCard = ({ exercise }) => {
   return (
-    <div>ExerciseCard</div>
-  )
-}
+    <Link to={`exercise/${exercise.id}`} className="exercise-card">
+      <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
+      <Stack direction={"row"}>
+        <Button
+          sx={{
+            ml: "21px",
+            color: "#fff",
+            background: "#ffa9a9",
+            fontSize: "14px",
+            borderRadius: "20px",
+            textTransform: "capitalize",
+            p: "5px",
+          }}
+        >
+          {exercise.bodyPart}
+        </Button>
+        <Button
+          sx={{
+            ml: "21px",
+            color: "#fff",
+            background: "#fcc757",
+            fontSize: "14px",
+            borderRadius: "20px",
+            textTransform: "capitalize",
+            p: "5px",
+          }}
+        >
+          {exercise.target}
+        </Button>
+      </Stack>
+      <Typography
+        ml="21px"
+        color="#000"
+        fontWeight={"bold"}
+        mt="11px"
+        pb="10px"
+        textTransform="capitalize"
+        fontSize={"22px"}
+      >
+        {exercise.name}
+      </Typography>
+    </Link>
+  );
+};
 
-export default ExerciseCard
+export default ExerciseCard;
--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Pagination from "@mui/material/Pagination";
+import { Box, Stack, Typography } from "@mui/material/";
+import { exercisesOptions, fetchData } from "../utils/fetchData";
+import ExerciseCard from "./ExerciseCard";
 
-const Exercises = () => {
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const exercisesPerPage = 9;
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+  const currentExercises = exercises.slice(
+    indexOfFirstExercise,
+    indexOfLastExercise
+  );
+  console.log("bodypart check", bodyPart);
+  useEffect(() => {
+    const fetchExercisesData1 = async () => {
+      console.log("fucntion called")
+      let exercisesData = [];
+      if (bodyPart === "all") {
+        exercisesData = await fetchData(
+          "https://exercisedb.p.rapidapi.com/exercises",
+          exercisesOptions
+        );
+      } else {
+        exercisesData = await fetchData(
+          `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+          exercisesOptions
+        );
+      }
+      console.log("getting the data");
+      console.log("exercisesData", exercisesData);
+      setExercises(exercisesData);
+    };
+    fetchExercisesData1();
+  }, [bodyPart]);
   return (
-    <div>Exercises</div>
-  )
-}
+    <Box id="exercises" sx={{ mt: { lg: "110px" } }} mt="50px" p="20px">
+      <Typography variant="h4">Showing Results</Typography>
+      <Stack
+        direction={"row"}
+        sx={{ gap: { lg: "110px", xs: "50px" } }}
+        flexWrap={"wrap"}
+        justifyContent={"center"}
+      >
+        {currentExercises.map((exercise, index) => (
+          <ExerciseCard key={index} exercise={exercise} />
+        ))}
+      </Stack>
+    </Box>
+  );
+};
 
-export default Exercises
+export default Exercises;
+
